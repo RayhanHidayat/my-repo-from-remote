a5e992b99aef9df6f47619878ff4edd655fa5d62
rayhanHidayat <rayhananh2001@gmail.com>
2020-09-27 10:16:14 +0700

    added first commit

---
+++ Assets/Scripts/endTrigger.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class endTrigger : MonoBehaviour
+{
+
+    public gameManager GameManager;
+
+    void OnTriggerEnter()
+    {
+        GameManager.CompleteLevel();
+    }
+}
---
+++ Assets/Scripts/playerCollision.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class playerCollision : MonoBehaviour
+{
+
+    public playerMovement movement;
+
+    void OnCollisionEnter( Collision collisionInfo)
+    {
+        if ( collisionInfo.collider.tag == "Obstacle" )
+        {
+            movement.enabled = false; 
+            FindObjectOfType<gameManager>().EndGame();      
+        }
+    }
+}
