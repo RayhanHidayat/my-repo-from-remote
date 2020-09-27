bdcdfd5672a6e507ed085d2b1d644577af543030
rayhanHidayat <rayhananh2001@gmail.com>
2020-09-27 10:18:10 +0700

    added comment in levelComplete

--- Assets/Scripts/levelComplete.cs
+++ Assets/Scripts/levelComplete.cs
@@ -3,6 +3,7 @@ using UnityEngine.SceneManagement;
 
 public class levelComplete : MonoBehaviour
 {
+    // Function for load next level
     public void loadNextLevel()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
