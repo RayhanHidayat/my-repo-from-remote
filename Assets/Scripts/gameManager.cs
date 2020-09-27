3288b4246b257653abc173fa4dcd2c566b458bc7
rayhanHidayat <rayhananh2001@gmail.com>
2020-09-27 10:42:03 +0700

    erase the game over print and added new comments

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -9,23 +9,25 @@ public class gameManager : MonoBehaviour
 
     public GameObject completeLevelUI;
 
+    // Activating UI Level Complete 
     public void CompleteLevel()
     {
         completeLevelUI.SetActive(true);
     }
 
+    // Making game over with restarting the game again
     public void EndGame()
     {
         if (gameHasEnded == false)
         {
             gameHasEnded = true;
-            Debug.Log("Game Over");
             // For restarting the game
             Invoke("Restart", restartDelay);
         }
         
     }
 
+    // For restarting the game
     void Restart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
