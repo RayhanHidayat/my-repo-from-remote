171a98c79013cdd3a1d376bd2695868509ae5631
rayhanHidayat <rayhananh2001@gmail.com>
2020-09-27 11:52:40 +0700

    commit new update

---
+++ Assets/Scripts/credits.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class credits : MonoBehaviour
+{
+    //Added Quit from game
+    public void Quit(){
+        //Application.Quit();
+        Debug.Log("Quit");
+    }
+}
